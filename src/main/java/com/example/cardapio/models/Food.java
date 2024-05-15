9fca6834bbe775d4e9b60f716cd4801e2f84b5cd
LucasG1020 <lucas.g.oliveira1@hotmail.com>
2024-05-15 11:23:45 -0300

    feat: route to add new food

--- src/main/java/com/example/cardapio/models/Food.java
+++ src/main/java/com/example/cardapio/models/Food.java
@@ -1,20 +1,38 @@
 package com.example.cardapio.models;
 
+import com.example.cardapio.controller.FoodResponseDTO;
+import com.example.cardapio.dto.foodDto.FoodRequestDTO;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Food
  */
 @Table(name = "foods")
 @Entity(name= "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
   @Id  @GeneratedValue(strategy =  GenerationType.IDENTITY)
-  private long id;
+  private Long id;
   private String title;
   private String image;
-  private Integer price;
+  private Double price;
+
+  public Food(FoodRequestDTO foodRequestDTO){
+    this.title = foodRequestDTO.title();
+    this.image = foodRequestDTO.image();
+    this.price = foodRequestDTO.price();
+  }
 }
